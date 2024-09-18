5ced28ee665563c7b6e306989e223d61f98324a6
GreenThunder <123633616+SamuelAyres76@users.noreply.github.com>
2024-09-18 09:58:53 +0100

    helo

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, {Application, Request, Response} from "express" ;
+import morgan from "morgan";
+
+const PORT = process.env.PORT || 7603;
+
+const app: Application = express();
+
+app.get("/ping", async (_req : Request, res: Response) => 
+    {
+    res.send
+        ({
+        message: "Samuel Ayres S00237258",
+        });
+    });
+    
+     app.listen(PORT, () => 
+        {
+        console.log("Server is running on port  --", PORT);
+        });
+    
+app.get('/bananas', async (_req : Request, res: Response) =>
+    res.send('hello world, this is bananas'));
+  
+app.get('/Lemon', async (_req : Request, res: Response) =>
+    res.send('mmm lemon!'));
+
