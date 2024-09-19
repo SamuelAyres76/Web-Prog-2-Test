1f73cc005c54a733b58d3e767242cdd6b585158f
GreenThunder <123633616+SamuelAyres76@users.noreply.github.com>
2024-09-19 16:29:45 +0100

    Worksheet 2

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,5 @@ app.get("/ping", async (_req : Request, res: Response) =>
 app.get('/bananas', async (_req : Request, res: Response) =>
     res.send('hello world, this is bananas'));
   
-app.get('/Lemon', async (_req : Request, res: Response) =>
-    res.send('mmm lemon!'));
-
+app.get('/lemon', async (_req : Request, res: Response) =>
+    res.send('999x Lemon for €2.99!'));
